b637e6dd0ccd5b74c5ffd6684fccea3031758363
Msdian_Mukesh07 <mukeshram11999@gmail.com>
2025-05-29 20:11:50 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,21 @@
 
+<?php
+$host = "mysqlcnt";
+$username = "root";
+$password = "pwd@12345"; // your MySQL password
+$database = "users";
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
+     
+    
+
+        
+
+    
